e17bd062b193d971dc1236ef5961808766308dd6
Matthew Hummer <biggreyson@gmail.com>
2021-12-11 14:48:15 -0700

    render all rooms

--- client/pathfinder/renderer.go
+++ client/pathfinder/renderer.go
@@ -11,9 +11,9 @@ import (
 var (
   ROOM_BLOCK_SIZE = float64(32.0)
   ROOM_BLOCK_MID = float64(ROOM_BLOCK_SIZE / 2.0)
-  PADDING = float64(5.0)
+  PADDING = float64(10.0)
   MAX_ROOMS = float64(9.0)
-  WINDOW_DIM = float64((ROOM_BLOCK_SIZE + PADDING) * MAX_ROOMS * 2.0)
+  WINDOW_DIM = float64((ROOM_BLOCK_SIZE + PADDING) * MAX_ROOMS * 1.2)
   WINDOW_MID = WINDOW_DIM / float64(2.0)
   HALF_PAD = PADDING / float64(2.0)
   CONNECTION_WIDTH = float64(6.0)
@@ -37,10 +37,6 @@ func GetTopRight(room *game.Room) pixel.Vec {
 func DrawRoom(room *game.Room, target pixel.Target) {
   bottomLeft := GetBottomLeft(room)
   topRight := GetTopRight(room)
-  color := colornames.Darkslategray
-  if room.IsFirstRoom {
-    color = colornames.Red
-  }
 
   if room.HasUpDoor() { // TOP
     blx := topRight.X - ROOM_BLOCK_MID - HALF_CONNECTION_WIDTH
@@ -52,17 +48,46 @@ func DrawRoom(room *game.Room, target pixel.Target) {
     game.DrawRect(target, colornames.Black, pixel.V(blx, bly), pixel.V(trx, try))
   }
 
-  // if room.HasRightDoor() { // RIGHT
-  //   game.DrawRect(target, colornames.Black, pixel.V(0,0), pixel.V(100, 100))
-  // }
+  if room.HasRightDoor() { // RIGHT
+    blx := topRight.X
+    bly := topRight.Y - ROOM_BLOCK_MID - HALF_CONNECTION_WIDTH
 
-  // if room.HasDownDoor() { // BOTTOM
-  //   game.DrawRect(target, colornames.Black, pixel.V(0,0), pixel.V(100, 100))
-  // }
+    trx := blx + PADDING
+    try := bly + CONNECTION_WIDTH
 
-  // if room.HasLeftDoor() { // LEFT
-  //   game.DrawRect(target, colornames.Black, pixel.V(0,0), pixel.V(100, 100))
-  // }
+    game.DrawRect(target, colornames.Black, pixel.V(blx, bly), pixel.V(trx, try))
+  }
+
+  if room.HasDownDoor() { // BOTTOM
+    blx := bottomLeft.X + ROOM_BLOCK_MID - HALF_CONNECTION_WIDTH
+    bly := bottomLeft.Y - PADDING
+
+    trx := bottomLeft.X + ROOM_BLOCK_MID + HALF_CONNECTION_WIDTH
+    try := bottomLeft.Y
+
+    // NOTE: I suspect this might not always work. Especially if it's not a flat dungeon.
+    color := colornames.Black
+    if room.IsFirstRoom {
+      color = colornames.Yellow
+    }
+
+    game.DrawRect(target, color, pixel.V(blx, bly), pixel.V(trx, try))
+  }
+
+  if room.HasLeftDoor() { // LEFT
+    blx := bottomLeft.X - PADDING
+    bly := bottomLeft.Y + ROOM_BLOCK_MID - HALF_CONNECTION_WIDTH
+
+    trx := bottomLeft.X
+    try := bottomLeft.Y + ROOM_BLOCK_MID + HALF_CONNECTION_WIDTH
+
+    game.DrawRect(target, colornames.Black, pixel.V(blx, bly), pixel.V(trx, try))
+  }
+
+  color := colornames.Darkslategray
+  if room.IsFirstRoom {
+    color = colornames.Red
+  }
 
   game.DrawRect(target, color, bottomLeft, topRight)
 }
